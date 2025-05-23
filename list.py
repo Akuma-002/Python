de768a0cbfa71fc878b0d25fdeaeec7baae8e963
SK SAHIL UDDIN <151388592+Akuma-002@users.noreply.github.com>
2025-05-23 11:33:31 +0530

    data

--- list.py
+++ list.py
@@ -1,2 +1,5 @@
 li = ["sahil", 2, 345, '2', True]
-print(li.index(78))  # returns the index of the first occurrence of the value
+print(type(li))
+
+t = (1, 3, "sa", True)
+print(len(t))
--- new.py
+++ new.py
@@ -1,1 +1,7 @@
-import pip 
+dictionary = {
+    'name': "John",
+    'age': 30,
+    'city': "New York"
+}
+
+print(type(dictionary))
