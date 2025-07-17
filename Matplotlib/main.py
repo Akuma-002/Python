06eb4fe760d29e6dff8828e9552fdbba668823b0
SK SAHIL UDDIN <151388592+Akuma-002@users.noreply.github.com>
2025-07-17 06:28:36 +0530

    Title, X, Y

--- Matplotlib/main.py
+++ Matplotlib/main.py
@@ -2,12 +2,17 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 df = pd.read_csv("data\profit_data.csv")
+df2 = pd.read_csv("data\profit_data_2.csv")
 #print(df)
 a = df["Day"]
 print(a)
 
 x = a.to_list()
 y = df["Profit"].to_list()
-
+z = df2["Profit"].to_list()
 plt.plot(x, y)
+plt.plot(x, z)
+plt.xlabel("Days")
+plt.ylabel("Profit")
+plt.title("Sales report")
 plt.show()
