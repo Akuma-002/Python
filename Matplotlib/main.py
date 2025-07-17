fbe7faad67daa60ea9c9852341a1bfae45213271
SK SAHIL UDDIN <151388592+Akuma-002@users.noreply.github.com>
2025-07-17 05:55:43 +0530

    New Data

--- Matplotlib/main.py
+++ Matplotlib/main.py
@@ -1,7 +1,13 @@
 import pandas as pd
-import matplotlib as plt
+import matplotlib.pyplot as plt
 
 df = pd.read_csv("data\profit_data.csv")
-print(df)
+#print(df)
 a = df["Day"]
 print(a)
+
+x = a.to_list()
+y = df["Profit"].to_list()
+
+plt.plot(x, y)
+plt.show()
