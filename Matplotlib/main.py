29c9c3b798db92402ca2597cef76e1b82016506d
SK SAHIL UDDIN <151388592+Akuma-002@users.noreply.github.com>
2025-07-17 05:49:33 +0530

    New Data

--- Matplotlib/main.py
+++ Matplotlib/main.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import matplotlib as plt
 
-df = pd.read_csv("data\employee_data.csv")
+df = pd.read_csv("data\profit_data.csv")
 print(df)
-print(df.head())
+a = df["Day"]
+print(a)
