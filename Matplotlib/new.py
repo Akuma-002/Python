56810a64cef3a2f1ec0ca6f6f4cd1f3e6ebd1f7f
SK SAHIL UDDIN <151388592+Akuma-002@users.noreply.github.com>
2025-07-19 12:38:10 +0530

    Method 2

--- Matplotlib/new.py
+++ Matplotlib/new.py
@@ -1,11 +1,4 @@
 a = int(input("Enter the first number : "))
 b = int(input("Enter the second number : "))
 
-if(a == b):
-    print("Both are equal")
-elif(a < b):
-    print(f"{a} is smaller then {b}")
-elif(a > b):
-    print(f"{b} is smaller then {a}")
-else:
-    print("Invalid")
+print(f"{min(a, b)} is smaller")
