19acbc710d9a4db73d568ca804cd4972d0e5a5d6
SK SAHIL UDDIN <151388592+Akuma-002@users.noreply.github.com>
2025-07-21 11:04:25 +0530

    Update

--- Machine Learning/main.py
+++ Machine Learning/main.py
@@ -1,5 +1,5 @@
 import pandas as pd
-
+import matplotlib.pyplot as plt
 df = pd.read_csv("data\salary_prediction_dataset_2000.csv")
 
 df2 = df.dropna()
@@ -7,5 +7,11 @@ print(df.head())
 print(df2.head())
 print(df.shape)
 print(df2.shape)
+nullV = (df.isnull().sum().sum())
+notnullV = (df.shape[0] * df.shape[1]) - (df.isnull().sum().sum())
 print(df.shape[0] * df.shape[1])
 print((df.isnull().sum().sum())/df.shape[0] * df.shape[1])
+val = [nullV, notnullV]
+name = ["null", "not Null"]
+plt.pie(val,labels=name, autopct="%1.1f%%")
+plt.show()
