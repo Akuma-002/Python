83cd5164a5ffe357d126f3f5d33c40ecea96028d
SK SAHIL UDDIN <151388592+Akuma-002@users.noreply.github.com>
2025-07-30 22:32:39 +0530

    factorial

--- test.py
+++ test.py
@@ -1,11 +1,6 @@
 num = int(input("Enter a number: "))
-flag = 0
+res = num
 for i in range(1, num):
-    if (flag > 1 or num <=0):
-        break
-    if(num % i == 0):
-        flag+=1
-if(flag == 1):
-    print("Prime")
-else:
-    print("Not Prime")
+    res*= i
+
+print(res)
