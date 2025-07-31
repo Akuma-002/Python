6df4b8c00ed9d09a5bd0529b440fdeecbe2511ee
SK SAHIL UDDIN <151388592+Akuma-002@users.noreply.github.com>
2025-07-31 16:10:04 +0530

    Update

--- Matplotlib/new.py
+++ Matplotlib/new.py
@@ -1,14 +1,2 @@
-import numpy as np
-import pandas as pd
-
-a = {
-    "name" : ["sahil", "Ram"],
-    "marks" : [12, 34]
-}
-
-print(type(a))
-
-ar = pd.DataFrame(a)
-print(ar)
-print(ar.iloc[1])
-
+print("s", end=" ")
+print("s")
--- test.py
+++ test.py
@@ -1,6 +1,17 @@
-num = int(input("Enter a number: "))
-res = num
-for i in range(1, num):
-    res*= i
+n = 5
+for i in range(n+1):
+    print("*"*i)
 
-print(res)
+
+for i in range(n+1):
+    print(" "*(n-i), "*"*i)
+    
+    
+    
+
+count = 1
+for i in range(n):
+    for j in range(i+1):
+        print(count, end=" ")
+        count+=1
+    print("\n")
