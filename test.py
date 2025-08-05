f2c4ae0a2edc52731122765311f6a409ba36d02c
SK SAHIL UDDIN <151388592+Akuma-002@users.noreply.github.com>
2025-08-05 16:35:52 +0530

    list

--- test.py
+++ test.py
@@ -1,24 +1,29 @@
-n = 5
-for i in range(n+1):
-    print("*"*i)
+# n = 5
+# for i in range(n+1):
+#     print("*"*i)
 
 
-for i in range(n+1):
-    print(" "*(n-i), "*"*i)
+# for i in range(n+1):
+#     print(" "*(n-i), "*"*i)
     
     
     
 
-count = 1
-for i in range(n):
-    for j in range(i+1):
-        print(count, end=" ")
-        count+=1
-    print("\n")
+# count = 1
+# for i in range(n):
+#     for j in range(i+1):
+#         print(count, end=" ")
+#         count+=1
+#     print("\n")
     
     
-for i in range(n+1):
-    print(" "*(n-i),"*"*((i*2)-1))
+# for i in range(n+1):
+#     print(" "*(n-i),"*"*((i*2)-1))
 
-str = "Hello'''"
-print(str.rstrip("'"))
+# str = "Hello'''"
+# print(str.rstrip("'"))
+
+a = [1, 2 , 3, 4, 5, 6, 7, 8]
+
+for i in a:
+    print(i)
