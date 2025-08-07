561663d2cc7e22884d91a692c06b1b9725d84889
SK SAHIL UDDIN <151388592+Akuma-002@users.noreply.github.com>
2025-08-07 15:59:39 +0530

    Update

--- test.py
+++ test.py
@@ -23,21 +23,47 @@
 # str = "Hello'''"
 # print(str.rstrip("'"))
 
-a = [1, 2 , 3, 4, 5, 6, 7, "sahil"]
-d = ("sahil", 12)
-s = {
-    "name": "Sahil",
-    "age" : 18
-}
-t = 123456789033
-print(t)
-print(d)
-for i in a:
-    print(i)
+# a = [1, 2 , 3, 4, 5, 6, 7, "sahil"]
+# d = ("sahil", 12)
+# s = {
+#     "name": "Sahil",
+#     "age" : 18
+# }
+# t = 123456789033
+# print(t)
+# print(d)
+# for i in a:
+#     print(i)
     
 
-a.append("Ram")
+# a.append("Ram")
 
-for i in a:
-    print(i)
+# for i in a:
+#     print(i)
     
+    
+# a = "sahil"
+# for i in range(1, len(a)+1):
+#     print(a[-i], end='')
+    
+
+
+
+# #Next
+# na = input("Enter a word: ")
+# count = 0
+# for i in na:
+#     if (i == "a"or i=='e' or i == 'i' or i == 'o' or i == 'u'):
+#         count+=1
+# print(f"Total vowals are : {count}")
+
+
+st = input("Enter a string: ")
+count = 0
+for i in range(0, int(len(st)/2)):
+    if(st[i] == st[(-(i+1))]):
+        count += 1
+if(count == int(len(st)/2)):
+    print("Palindrome")
+else:
+    print("Not Palindrome")
