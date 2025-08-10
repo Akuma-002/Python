927678249eee6cd46766208afe25335c8fcbfe14
SK SAHIL UDDIN <151388592+Akuma-002@users.noreply.github.com>
2025-08-11 02:08:40 +0530

    Check Outliers

--- Machine Learning/test.py
+++ Machine Learning/test.py
@@ -1,5 +1,9 @@
 import pandas as pd
+import seaborn as sns
 from sklearn.preprocessing import OrdinalEncoder
 import matplotlib.pyplot as plt
-df = pd.read_csv("data\insurance_data_2000.csv")
-print(df)
+df = pd.read_csv("data\outlier_test_data_2000.csv")
+print(df.describe())
+
+sns.boxplot(df["Feature1"])
+plt.show()
