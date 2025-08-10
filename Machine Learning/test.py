942c0bfa960d3b711e095b0f17c9dfe586a574f4
SK SAHIL UDDIN <151388592+Akuma-002@users.noreply.github.com>
2025-08-10 23:38:41 +0530

    Update

--- Machine Learning/test.py
+++ Machine Learning/test.py
@@ -3,4 +3,8 @@ from sklearn.preprocessing import LabelEncoder
 df = pd.DataFrame({"name": ["cow", "dog", "cat", "horse"] })
 le = LabelEncoder()
 df["en_name"] = le.fit_transform(df["name"])
-print(df)
+#print(df)
+df2 = pd.read_csv("data\insurance_data_2000.csv")
+
+df2["en_region"] = le.fit_transform(df2["region"])
+print(df2["en_region"].unique())
