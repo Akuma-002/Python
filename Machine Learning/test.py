46004f6c33a63bc20907fc84b6a73301200bd681
SK SAHIL UDDIN <151388592+Akuma-002@users.noreply.github.com>
2025-08-10 21:41:32 +0530

    Scikit learn

--- Machine Learning/test.py
+++ Machine Learning/test.py
@@ -1,7 +1,2 @@
 import pandas as pd
-df = pd.read_csv("data\sample_2000_rows.csv")
-print(df)
-print((df.isnull().sum()/len(df))*100)
-
-df = df.dropna()
-print((df.isnull().sum()/len(df))*100)
+df = pd.DataFrame({"name": ["cow", "dog", "cat", "horse"], })
