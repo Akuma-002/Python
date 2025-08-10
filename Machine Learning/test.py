ec307e8f320bec5a3d6d56091b00591f98f88734
SK SAHIL UDDIN <151388592+Akuma-002@users.noreply.github.com>
2025-08-10 22:10:49 +0530

    Simple Encoding

--- Machine Learning/test.py
+++ Machine Learning/test.py
@@ -3,5 +3,4 @@ from sklearn.preprocessing import LabelEncoder
 df = pd.DataFrame({"name": ["cow", "dog", "cat", "horse"] })
 le = LabelEncoder()
 df["en_name"] = le.fit_transform(df["name"])
-df
 print(df)
