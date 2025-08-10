fdb5edf390b93a8d88dcb25045eb029e6fcca847
SK SAHIL UDDIN <151388592+Akuma-002@users.noreply.github.com>
2025-08-10 21:54:24 +0530

    Label Encoder

--- Machine Learning/test.py
+++ Machine Learning/test.py
@@ -1,2 +1,7 @@
 import pandas as pd
-df = pd.DataFrame({"name": ["cow", "dog", "cat", "horse"], })
+from sklearn.preprocessing import LabelEncoder
+df = pd.DataFrame({"name": ["cow", "dog", "cat", "horse"] })
+le = LabelEncoder()
+df["en_name"] = le.fit_transform(df["name"])
+df
+print(df)
