aab80712f15ca7347aa8b865ccb15cffabcbaeb8
SK SAHIL UDDIN <151388592+Akuma-002@users.noreply.github.com>
2025-08-11 02:28:54 +0530

    Outlier remove using IQR

--- Machine Learning/test.py
+++ Machine Learning/test.py
@@ -3,7 +3,15 @@ import seaborn as sns
 from sklearn.preprocessing import OrdinalEncoder
 import matplotlib.pyplot as plt
 df = pd.read_csv("data\outlier_test_data_2000.csv")
-print(df.describe())
+q1 = df['Feature1'].quantile(0.25)
+q3 = df['Feature1'].quantile(0.75)
+IQR = q3 - q1
+min_range = q1 - (1.5*IQR)
+max_range = q3 + (1.5*IQR)
+n_df = df[(df['Feature1']<=max_range) & (df['Feature1']>= min_range)]
+print(df.shape)
+print(n_df.shape)
 
-sns.boxplot(df["Feature1"])
+sns.displot( x = "Feature1", data=df)
+sns.displot( x = "Feature1", data=n_df)
 plt.show()
