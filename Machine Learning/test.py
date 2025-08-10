e7c4768e52c85e686427f70ee718e1ef35ca6112
SK SAHIL UDDIN <151388592+Akuma-002@users.noreply.github.com>
2025-08-11 01:40:33 +0530

    Update

--- Machine Learning/test.py
+++ Machine Learning/test.py
@@ -1,10 +1,5 @@
 import pandas as pd
-from sklearn.preprocessing import LabelEncoder
-df = pd.DataFrame({"name": ["cow", "dog", "cat", "horse"] })
-le = LabelEncoder()
-df["en_name"] = le.fit_transform(df["name"])
-#print(df)
-df2 = pd.read_csv("data\insurance_data_2000.csv")
-
-df2["en_region"] = le.fit_transform(df2["region"])
-print(df2["en_region"].unique())
+from sklearn.preprocessing import OrdinalEncoder
+import matplotlib.pyplot as plt
+df = pd.read_csv("data\insurance_data_2000.csv")
+print(df)
