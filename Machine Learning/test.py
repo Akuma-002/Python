875f4ff42eaf32cccb99f097a3cc2f2053a9df74
SK SAHIL UDDIN <151388592+Akuma-002@users.noreply.github.com>
2025-08-11 11:12:57 +0530

    Outlier removal using Z score

--- Machine Learning/test.py
+++ Machine Learning/test.py
@@ -1,17 +1,6 @@
 import pandas as pd
-import seaborn as sns
-from sklearn.preprocessing import OrdinalEncoder
-import matplotlib.pyplot as plt
 df = pd.read_csv("data\outlier_test_data_2000.csv")
-q1 = df['Feature1'].quantile(0.25)
-q3 = df['Feature1'].quantile(0.75)
-IQR = q3 - q1
-min_range = q1 - (1.5*IQR)
-max_range = q3 + (1.5*IQR)
-n_df = df[(df['Feature1']<=max_range) & (df['Feature1']>= min_range)]
-print(df.shape)
-print(n_df.shape)
+z_score = (df["Feature1"] - df["Feature1"].mean())/(df["Feature1"].std())
+df["z_score"] = z_score
+new_df = df[(df["z_score"] <3) & (df["z_score"]>(-3))]
 
-sns.displot( x = "Feature1", data=df)
-sns.displot( x = "Feature1", data=n_df)
-plt.show()
