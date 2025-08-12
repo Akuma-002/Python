7c4104dc027b3837fb9f40bc75b4e46fc9afbdc2
SK SAHIL UDDIN <151388592+Akuma-002@users.noreply.github.com>
2025-08-12 16:54:30 +0530

    Data Cleaning

--- Machine Learning/test.py
+++ Machine Learning/test.py
@@ -1,6 +1,10 @@
 import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt 
 df = pd.read_csv("data\outlier_test_data_2000.csv")
+#sns.boxplot(x = "Feature1", data=df)
 z_score = (df["Feature1"] - df["Feature1"].mean())/(df["Feature1"].std())
 df["z_score"] = z_score
 new_df = df[(df["z_score"] <3) & (df["z_score"]>(-3))]
-
+sns.displot(x = "Feature1", data=df)
+plt.show()
