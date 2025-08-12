cddc17a5824839005006b27499863f37fb33ea17
SK SAHIL UDDIN <151388592+Akuma-002@users.noreply.github.com>
2025-08-12 17:07:10 +0530

    Outlier

--- Machine Learning/test.py
+++ Machine Learning/test.py
@@ -1,10 +1,11 @@
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt 
-df = pd.read_csv("data\outlier_test_data_2000.csv")
+df = pd.read_csv("data\outlier_sample_1000000.csv")
 #sns.boxplot(x = "Feature1", data=df)
 z_score = (df["Feature1"] - df["Feature1"].mean())/(df["Feature1"].std())
 df["z_score"] = z_score
-new_df = df[(df["z_score"] <3) & (df["z_score"]>(-3))]
-sns.displot(x = "Feature1", data=df)
+print(df["z_score"].std())
+new_df = df[(df["z_score"] <df["z_score"].std()) & (df["z_score"]>(-(df["z_score"].std())))]
+sns.displot(x = "z_score", data=new_df)
 plt.show()
