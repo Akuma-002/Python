abbfa7f957e240842ad0c96ea327666f43c1a933
SK SAHIL UDDIN <151388592+Akuma-002@users.noreply.github.com>
2025-08-13 22:04:27 +0530

    Standardization

--- Machine Learning/test.py
+++ Machine Learning/test.py
@@ -1,12 +1,11 @@
 import pandas as pd
 import seaborn as sns
-import matplotlib.pyplot as plt 
+import matplotlib.pyplot as plt
+from sklearn.preprocessing import StandardScaler
 df = pd.read_csv("data\outlier_sample_1000000.csv")
-#sns.boxplot(x = "Feature1", data=df)
-z_score = (df["Feature1"] - df["Feature1"].mean())/(df["Feature1"].std())
-df["z_score"] = z_score
-print(df["z_score"].std())
-new_df = df[(df["z_score"] <df["z_score"].std()) & (df["z_score"]>(-(df["z_score"].std())))]
-sns.displot(x = "z_score", data=new_df)
+ss = StandardScaler()
+ss.fit(df[["Feature1"]])
+df["Feature4"] = pd.DataFrame(ss.transform(df[["Feature1"]]), columns=["x"])
+print(df)
+sns.displot(df["Feature4"])
 plt.show()
-
