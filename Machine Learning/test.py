0755b6c5ae0f480bdcae07fe51d717ce59a8c919
SK SAHIL UDDIN <151388592+Akuma-002@users.noreply.github.com>
2025-08-14 08:51:03 +0530

    FunctionTransformer

--- Machine Learning/test.py
+++ Machine Learning/test.py
@@ -1,4 +1,8 @@
 import pandas as pd
+import numpy as np
+from sklearn.preprocessing import FunctionTransformer
 df = pd.DataFrame({"name":["Ram", "Sam", "sahil", "Sam"], "age": [12, 14, 18, 14]})
-df = df.drop_duplicates()
+ft = FunctionTransformer(func=np.log1p)
+ft.fit(df["age"])
+df["new"] = ft.transform(df["age"])
 print(df)
