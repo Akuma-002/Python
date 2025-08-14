a83a4cce097ad92a68e633b83543f61623d5e907
SK SAHIL UDDIN <151388592+Akuma-002@users.noreply.github.com>
2025-08-14 08:00:09 +0530

    MinMaxScaler

--- Machine Learning/test.py
+++ Machine Learning/test.py
@@ -1,9 +1,9 @@
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
-from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import MinMaxScaler
 df = pd.read_csv("data\outlier_sample_1000000.csv")
-ss = StandardScaler()
+ss = MinMaxScaler ()
 ss.fit(df[["Feature1"]])
 df["Feature4"] = pd.DataFrame(ss.transform(df[["Feature1"]]), columns=["x"])
 print(df)
