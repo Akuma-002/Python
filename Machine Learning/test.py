978379dbac6dabc6c7d5d0e692beeb2f2ce72007
SK SAHIL UDDIN <151388592+Akuma-002@users.noreply.github.com>
2025-08-14 08:17:17 +0530

    Drop Duplicates

--- Machine Learning/test.py
+++ Machine Learning/test.py
@@ -1,11 +1,4 @@
 import pandas as pd
-import seaborn as sns
-import matplotlib.pyplot as plt
-from sklearn.preprocessing import MinMaxScaler
-df = pd.read_csv("data\outlier_sample_1000000.csv")
-ss = MinMaxScaler ()
-ss.fit(df[["Feature1"]])
-df["Feature4"] = pd.DataFrame(ss.transform(df[["Feature1"]]), columns=["x"])
+df = pd.DataFrame({"name":["Ram", "Sam", "sahil", "Sam"], "age": [12, 14, 18, 14]})
+df = df.drop_duplicates()
 print(df)
-sns.displot(df["Feature4"])
-plt.show()
