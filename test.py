5528256c6228e06dd22215fbcf76caf5a4f0dd62
SK SAHIL UDDIN <sksahilu735@gmail.com>
2025-08-28 14:13:56 +0530

    Question 2

--- test.py
+++ test.py
@@ -170,11 +170,38 @@
 # print(out)
 
 
-flowers = ["Blue-Spider Lili", "Sakura", "lavender"]
-flowers.append("Marigold")
-flowers.insert(2, "Rose")
-flowers.pop(2)
-flowers.sort(reverse=True)
-print(f"Count of Rose is {flowers.count("Rose")}")
-flowers[flowers.index("lavender")] = flowers[flowers.index("lavender")].upper()
-print(flowers)
+# flowers = ["Blue-Spider Lili", "Sakura", "Lavender"]
+# flowers.append("Marigold")
+# flowers.insert(2, "Rose")
+# flowers.pop(4)
+# flowers.sort(reverse=True)
+# print(f"Count of Rose is {flowers.count("Rose")}")
+# print(flowers[2].upper())
+# print(flowers)
+
+
+# lis = [1, 2, 2, 3, 3, 3, 4, 5, 4, 6, 2, 7, 2]
+# nLis = []
+
+# for i in lis:
+#     if i not in nLis:
+#         nLis.append(i)
+# lis = nLis
+# print(lis)
+
+lis = [1, 2, 3, 4, 5, 6 , 7]
+print("Max - ", max(lis))
+print("Min - ", min(lis))
+print("Average - ", (sum(lis)/len(lis)))
+print("Sum - ", sum(lis))
+lis.reverse()
+print(lis)
+lis2 = []
+for i in lis:
+    lis2.append(i*i)
+print(lis2)
+even_sum = 0
+for i in lis :
+    if i % 2 ==0 :
+        even_sum+=i
+print(even_sum)
