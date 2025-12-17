7fdcaa07d3a4abf542d58b54f5092c99a4d400b3
SK SAHIL UDDIN <sksahilu735@gmail.com>
2025-12-17 20:51:34 +0530

    Update

--- test.py
+++ test.py
@@ -1,19 +1,6 @@
-def cal_op(a,b,operation):
-    match operation:
-        case"+":
-            return a+b
-        case"-":
-            return a-b
-        case"*":
-            return a*b
-        case"/":
-            if b!=0:
-                return a/b
-            else:
-                return"error"
-            
-no1= float(input("enter first no.:"))
-no2= float(input("enter second no.:"))
-operator = input("enter operator(+,-,*,/):")
-result = cal_op(no1,no2,operator)
-print("Result:",result)
+greater = lambda a, b: a if a > b else b
+
+x = int(input("Enter first number: "))
+y = int(input("Enter second number: "))
+
+print("Greater number is:", greater(x, y))
