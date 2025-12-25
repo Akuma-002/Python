8e9a97b89ec1c6adb583b7b4d639ad02f1e11ff9
SK SAHIL UDDIN <sksahilu735@gmail.com>
2025-12-25 21:19:44 +0530

    Update

--- test.py
+++ test.py
@@ -1,6 +1,37 @@
-greater = lambda a, b: a if a > b else b
+lis = [5, 4, 3, 2, 1]
 
-x = int(input("Enter first number: "))
-y = int(input("Enter second number: "))
+# for i in range(len(lis)-1):
+#     flag = True
+#     for j in range(len(lis)-1-i):
+#         if (lis[j+1] < lis[j]):
+#             temp = lis[j+1]
+#             lis[j+1] = lis[j]
+#             lis[j] = temp
+#             flag = False
+#     if flag == True :
+#         break
+# print(lis)
 
-print("Greater number is:", greater(x, y))
+for i in range(len(lis)-1):
+    min_num = lis[i]
+    idx = i
+    for j in range(i, len(lis)):
+        if (lis[j] < min_num):
+            min_num = lis[j]
+            idx = j
+    temp = lis[idx]
+    lis[idx] = lis[i]
+    lis[i] = temp
+print(lis)
+
+# for i in range(1, len(lis)):
+#     key = lis[i]
+#     j = i-1
+    
+#     while j>=0 and key < lis[j]:
+#         lis[j + 1] = lis[j]
+#         j -= 1
+#     lis[j + 1] = key
+    
+# print(lis)
+
